2e329245b6837c43e2734c029c88f1509ad3f44a
PedroMaia21 <pedroabmaia@hotmail.com>
2026-06-05 12:02:59 +0100

    added function to store chunks

--- app/vector_store.py
+++ app/vector_store.py
@@ -3,3 +3,24 @@ import chromadb
 client = chromadb.PersistentClient(path="./chroma_db")
 
 collection = client.get_or_create_collection(name="knowledge_base")
+
+def store_chunks(chunks, embeddings, source):
+    ids = []
+    metadatas = []
+
+    for i, chunk in enumerate(chunks):
+        ids.append(f"{source}_{i}")
+
+        metadatas.append(
+            {
+                "source": source,
+                "chunk_index": i,
+            }
+        )
+
+    collection.add(
+        ids=ids,
+        documents=chunks,
+        embeddings=embeddings,
+        metadatas=metadatas
+    )
